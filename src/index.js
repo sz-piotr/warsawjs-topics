c286f51321a0e21f0a7532670837742b87678e2a
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-09-22 16:04:20 +0200

    Setup github login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const path = require('path')
+const passport = require('passport')
+const { Strategy: GitHubStrategy} = require('passport-github')
+
+passport.use(new GitHubStrategy(
+  {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "https://warsawjs-topics-test.herokuapp.com/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile)
+    return cb(profile)
+  })
+)
 
 const app = express()
 
@@ -10,5 +24,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function (req, res) {
   res.render('index')
 })
+
+app.get(
+  '/auth/github',
+  passport.authenticate('github')
+)
+
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/')
+  }
+)
 
 app.listen(process.env.PORT || 4000)
