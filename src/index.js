fb188436c501e673958744cd460717e3e908920d
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-09-22 15:52:20 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.listen(4000)
